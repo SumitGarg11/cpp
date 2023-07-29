3b16d435e7282bd0faa4029eee1d927c960c282b
sumit <garg10sumit@gmail.com>
2023-07-29 22:08:37 +0530

    integrated_development_enviroment

---
+++ integrated_development_enviroment.cpp
@@ -0,0 +1,7 @@
+An Integrated Development Environment (IDE) is a 
+piece of software that contains all of the things
+you need to 
+develop, 
+compile, 
+link, and 
+debug your programs.
--- introduction_compiler_linker.cpp
+++ introduction_compiler_linker.cpp
@@ -1,16 +1,16 @@
-object file --> C++ source code into a 
-machine language file
+// object file --> C++ source code into a 
+// machine language file
 
-If your program had 3 .cpp files, the compiler would 
-generate 3 object files:
+// If your program had 3 .cpp files, the compiler would 
+// generate 3 object files:
 
 
-  source file:     |     source file:
-calculator.cpp     |    fraction.cpp
-    |              |         |      
-    |              |         |
-  object file:     |     object file:
-calculator.o       |    fraction.o
+//   source file:     |     source file:
+// calculator.cpp     |    fraction.cpp
+//     |              |         |      
+//     |              |         |
+//   object file:     |     object file:
+// calculator.o       |    fraction.o
 
-C++ also comes with an extensive library 
-called the C++ Standard Library
+// C++ also comes with an extensive library 
+// called the C++ Standard Library
