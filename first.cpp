923d819e5e0f7cf951c9970d6aa7af617010d365
sumit <garg10sumit@gmail.com>
2023-07-29 20:24:28 +0530

    introduction of c++

--- first.cpp
+++ first.cpp
@@ -1,6 +1,2 @@
-#include <iostream>
 
-int main(void) {
-    std::cout << "Hello, world from first.cpp";
-    return 0;
-}
+
---
+++ introduction_03.cpp
@@ -0,0 +1,15 @@
+//C++ (pronounced see plus plus) was developed by Bjarne Stroustrup at Bell Labs 
+// as an extension to C, starting in 1979.
+
+//C++ is designed to allow the programmer a high degree of freedom to do what they want. 
+//Here are a few common types of applications that most likely would be written in C++:
+
+
+// 1.Video games
+// 2.Real-time systems
+// 3.Graphical applications and simulations
+// 4.Productivity / office applications
+// 5.Embedded software
+// 6.Audio and video processing
+// 7.Artificial intelligence and neural networks
+
