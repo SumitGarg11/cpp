fb648857c8243c0781cf4beac40b767d8ed6d062
sumit <garg10sumit@gmail.com>
2023-07-29 21:41:51 +0530

    introduction_compiler_linker

---
+++ shyam.c
@@ -0,0 +1,12 @@
+//C++ (pronounced see plus plus) was developed by Bjarne Stroustrup at Bell Labs 
+// as an extension to C, starting in 1979.
+
+//C++ is designed to allow the programmer a high degree of freedom to do what they want. 
+//Here are a few common types of applications that most likely would be written in C++:
+
+
+// 1.Video games
+// 2.Real-time systems
+// 3.Graphical applications and simulations
+// 4.Productivity / office applications
+// 5.Embedded software
