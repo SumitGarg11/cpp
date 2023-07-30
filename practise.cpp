4a58122833c4705c1b5fc557a654bbe51112416b
sumit <garg10sumit@gmail.com>
2023-07-30 17:09:54 +0530

    if_else

---
+++ practise.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a(9);
+    int username;
+    std::cout <<  a;
+    std:: cout << "hello" << endl << "world";
+    std::cout << "enter your username : ";
+    std :: cin >> username;
+    std::cout<< "user name is: " <<  username;
+    return 0;
+}
