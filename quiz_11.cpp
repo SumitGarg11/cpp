7d58887953a3095408545cf94f4d66f1cf30f4c5
sumit <garg10sumit@gmail.com>
2023-07-30 11:02:12 +0530

    quiz_basic_c++

---
+++ quiz_11.cpp
@@ -0,0 +1,23 @@
+What is a statement?
+ans --  statement is an instruction in a computer program 
+that tells the computer to perform an action.
+
+que 2 -- what is a function?
+ans -- A function is a collection of statements 
+that executes sequentially.
+
+
+Question #3
+
+What is the name of the function 
+that all programs must have?
+
+ans -- main
+
+Question #4
+
+What is a syntax error?
+
+ans -- A syntax error is a compiler error that 
+occurs at compile-time when your program
+ violates the grammar rules of the C++ language.
